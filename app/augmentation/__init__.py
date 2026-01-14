e536888aa65d0f93029230ea3adbb6db9cd6e33c
bdipesh3045 <bdipesh304@gmail.com>
2026-01-14 01:54:05 -0600

    1st Push

---
+++ app/augmentation/__init__.py
@@ -0,0 +1,4 @@
+# correct relative import
+from .describe_tiff import describe_tiff
+
+__all__ = ["describe_tiff"]
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+
+
+setup(
+    name="augmentation",
+    version="0.0.10",
+    description="An id generator that generated various types and lengths ids",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+
+    long_description_content_type="text/markdown",
+    url="...",
+    author="Dipesh Sharma",
+    author_email="dipeshsharma9800@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "numpy",
+        "rasterio"
+    ],
+   
+    python_requires=">=3.10",
+)
