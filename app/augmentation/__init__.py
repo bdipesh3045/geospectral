0404a575ee925bca3ef34583584ef68ff9d5f6dc
bdipesh3045 <bdipesh304@gmail.com>
2026-01-16 00:50:35 -0600

    Fixed Routing Issue

--- app/augmentation/__init__.py
+++ app/augmentation/__init__.py
@@ -1,9 +1,7 @@
-
-
 from .clipping import clip_raster_threaded, clip_raster_serial
 from .store import files_load
-
+from .operation import geometric_augmentations
 
 
 # This defines what gets imported if someone types "from augmentation import *"
-__all__ = ["clip_raster_threaded", "clip_raster_serial","files_load"]
+__all__ = ["clip_raster_threaded", "clip_raster_serial","files_load","geometric_augmentations"]
--- app/augmentation/store.py
+++ app/augmentation/store.py
@@ -1,11 +1,26 @@
 import os
 
-def files_load(file_path):
-    print(f"Loading files from: {file_path}")
-    """Load and return a list of file names in the given directory."""
+def files_load(directory_path):
+    print(f"Loading files from: {directory_path}")
+    
     try:
-        files = os.listdir(file_path)
-        return files
+        # Get list of filenames
+        filenames = os.listdir(directory_path)
+        
+        full_paths = []
+        for name in filenames:
+            # 1. Join directory + filename -> "tiles/clip_1.tif"
+            relative_path = os.path.join(directory_path, name)
+            
+            # 2. Convert to absolute system path -> "/Users/dipesh/.../tiles/clip_1.tif"
+            absolute_path = os.path.abspath(relative_path)
+            
+            # Optional: Filter out hidden Mac files like .DS_Store or check valid extension
+            if not name.startswith('.') and name.endswith('.tif'):
+                full_paths.append(absolute_path)
+        
+        return (full_paths, filenames)
+
     except FileNotFoundError:
-        print(f"The directory {file_path} does not exist.")
+        print(f"The directory {directory_path} does not exist.")
         return []
